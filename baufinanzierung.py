3af4b15c7c728d58d8234dde8de5bb935d7be92c
fgjhhkfk <fgjhhkfk@googlemail.com>
2016-04-18 22:24:44 +0200

    Variablennamen angepasst; Sondertilgung ab einem gegebenen Jahr mit festem Wert eingebaut.

--- baufinanzierung.py
+++ baufinanzierung.py
@@ -2,7 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-def zinsberechnung(K, Z, Rate, ST, zinsanpassung):
+def zinsberechnung(K, Z, Rate, sondertilgung, zinsanpassung):
     j = 0
     bez = 0
     res = np.array([K])
@@ -27,9 +27,14 @@ def zinsberechnung(K, Z, Rate, ST, zinsanpassung):
         K = K - Rate
 
         # Sondertilgung in den entsprechenden Jahren abziehen
-        if j in ST[0]:
-            K = K - ST[1][ST[0].index(j)]
-            bez = bez + ST[1][ST[0].index(j)]
+        if j in sondertilgung[0]:
+            K = K - sondertilgung[1][sondertilgung[0].index(j)]
+            bez = bez + sondertilgung[1][sondertilgung[0].index(j)]
+
+        # ab einem gegebenen Jahr immer die gleiche sondertilgung abziehen
+        if sondertilgung[2] and j >= sondertilgung[2][0] and sondertilgung[3][0]<K:
+            K = K - sondertilgung[3][0]
+            bez = bez + sondertilgung[3][0]
 
         res = np.append(res, K)
         j += 1
@@ -42,11 +47,11 @@ K1 = 100000
 T1 = 0.0336
 Z1 = 0.015
 zinsanpassung = ([13],[0.0215])
-ST1 = ([],[])
+sondertilgung = ([],[],[],[])
 Rate1 = K1*T1 + K1*Z1
 
 print "Rate fuer den 1. Kredit: " + str(Rate1) + "EUR. Das Entspricht " + str(Rate1/12) + "EUR monatlich"
-[res1, bez1] = zinsberechnung(K1, Z1, Rate1, ST1, zinsanpassung)
+[res1, bez1] = zinsberechnung(K1, Z1, Rate1, sondertilgung, zinsanpassung)
 print "Fuer den 1. Kredit bezahlt: " + str(bez1) + "EUR"
 plt.plot(res1, 'og', label='Modell 1: ' + str(K1/1000) + 'KEUR, T ' + str(T1*100) + '%, Z ' + str(Z1*100) + '%')
 
@@ -57,11 +62,11 @@ K2 = 100000
 T2 = 0.035
 Z2 = 0.02
 zinsanpassung = ([999],[0])
-ST2 = ([],[])
+sondertilgung = ([1,2,4,5],[3000, 3000, 3000, 3000],[8],[4500])
 Rate2 = K2*T2 + K2*Z2
 
 print "Rate fuer den 2. Kredit: " + str(Rate2) + "EUR. Das Entspricht " + str(Rate2/12) + "EUR monatlich"
-[res2, bez2] = zinsberechnung(K2, Z2, Rate2, ST2, zinsanpassung)
+[res2, bez2] = zinsberechnung(K2, Z2, Rate2, sondertilgung, zinsanpassung)
 print "Fuer den 2. Kredit bezahlt: " + str(bez2) + "EUR"
 plt.plot(res2, 'or', label='Modell 2: ' + str(K2/1000) + 'KEUR, T ' + str(T2*100) + '%, Z ' + str(Z2*100) + '%')
 
@@ -71,11 +76,11 @@ Kreditsumme = 100000
 Zins = 0.035
 Tilgung = 0.02
 zinsanpassung = ([999],[0])
-Sondertilgung = ([],[])
+sondertilgung = ([],[],[],[])
 Rate = Kreditsumme*Tilgung + Kreditsumme*Zins
 
 print "Rate fuer den x. Kredit: " + str(Rate) + "EUR. Das Entspricht " + str(Rate/12) + "EUR monatlich"
-[rrr, bbb] = zinsberechnung(Kreditsumme, Zins, Rate, Sondertilgung, zinsanpassung)
+[rrr, bbb] = zinsberechnung(Kreditsumme, Zins, Rate, sondertilgung, zinsanpassung)
 print "Fuer den x. Kredit bezahlt: " + str(bbb) + "EUR"
 plt.plot(rrr, 'ok', label='Modell 3: ' + str(Kreditsumme/1000) + 'KEUR, T ' + str(Tilgung*100) + '%, Z ' + str(Zins*100) + '%')
 
