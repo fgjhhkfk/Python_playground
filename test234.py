57fe7de2258b5ea8d132f153b029717edf3a8874
fgjhhkfk <fgjhhkfk@googlemail.com>
2016-05-01 23:54:48 +0200

    Mit einem Geradenfit herumgespielt

---
+++ test234.py
@@ -0,0 +1,23 @@
+from matplotlib import style
+import matplotlib.pyplot as plt
+from statistics import mean
+import numpy as np
+
+style.use('fivethirtyeight')
+
+x = np.array([1, 2, 3, 4, 5, 6, 7, 8, 9, 11])
+y = np.array([1.2, 3, 6, 2, 4, 3.4, 8, 7.6, 9, 6.5])
+
+# Geradengleichung: y = mx + b
+
+m = (mean(x*y) - mean(x)*mean(y)) / (mean(x**2) - (mean(x))**2)
+b = mean(y) - m*mean(x)
+print m
+print b
+
+geradenfit = [m*xn + b for xn in range(0, max(x), 1)]
+
+print max(x)
+plt.scatter(x, y)
+plt.plot(xrange(0, max(x), 1), geradenfit, color='r')
+plt.show()
