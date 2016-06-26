eb067ec156ebe1ffdabf9787432aba16b1597759
fgjhhkfk <fgjhhkfk@googlemail.com>
2016-06-26 23:00:09 +0200

    Eingelesener Text wird jetzt in lower case gewandelt; Ausgabe geaendert; Kommentar hinzugefuegt

--- language_deternimantion.py
+++ language_deternimantion.py
@@ -1,20 +1,21 @@
 import charcount as cc
 
-
-dEng = cc.chardistribution(cc.readfile('english.txt'))
-dGer = cc.chardistribution(cc.readfile('german.txt'))
-dEsp = cc.chardistribution(cc.readfile('spanish.txt'))
-dFra = cc.chardistribution(cc.readfile('french.txt'))
-Languages = [dEng, dGer, dEsp, dFra]
-
-##dCheck = cc.chardistribution(cc.readfile('nyt.txt'))
-##dCheck = cc.chardistribution(cc.readfile('zeit.txt'))
-##dCheck = cc.chardistribution(cc.readfile('elpais.txt'))
-##dCheck = cc.chardistribution(cc.readfile('lemonde.txt'))
-dCheck = cc.chardistribution(cc.readfile('repubblica.txt'))
+#%% 
+dEng = cc.chardistribution(cc.readfile('/home/hjk/Documents/english.txt').lower())
+dGer = cc.chardistribution(cc.readfile('/home/hjk/Documents/german.txt').lower())
+dEsp = cc.chardistribution(cc.readfile('/home/hjk/Documents/spanish.txt').lower())
+dFra = cc.chardistribution(cc.readfile('/home/hjk/Documents/french.txt').lower())
+Languages = {'Eng':dEng, 'Ger':dGer, 'Esp':dEsp, 'Fra':dFra}
+#%%
+#dCheck = cc.chardistribution(cc.readfile('/home/hjk/Documents/nyt.txt').lower())
+#dCheck = cc.chardistribution(cc.readfile('/home/hjk/Documents/zeit.txt').lower())
+#dCheck = cc.chardistribution(cc.readfile('/home/hjk/Documents/elpais.txt').lower())
+#dCheck = cc.chardistribution(cc.readfile('/home/hjk/Documents/lemonde.txt').lower())
+dCheck = cc.chardistribution(cc.readfile('/home/hjk/Documents/repubblica.txt').lower())
 
 ##delta = {key: [Language for Language in Languages][key] - dCheck[key] for key in dCheck.keys()}
-for refLang in Languages:
-    delta = {key: refLang[key] - dCheck[key] for key in dCheck.keys()}
-
-    print sum([abs(val) for val in delta.values()])
+print 'Je kleiner der Wert, desto wahrscheinlicher entspricht der untersuchte Text der Sprache.'
+for refLang in Languages.keys():
+    delta = {key: Languages[refLang][key] - dCheck[key] for key in dCheck.keys()}
+    #print dCheck.keys()
+    print refLang + ': ' + str(sum([abs(val) for val in delta.values()]))
