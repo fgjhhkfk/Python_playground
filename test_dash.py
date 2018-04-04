4921240cf7fb21b0a9c158cf6e8545da1eca16c4
fgjhhkfk <fgjhhkfk@googlemail.com>
2018-04-04 22:29:13 +0200

    immer weiter

--- test_dash.py
+++ test_dash.py
@@ -1,4 +1,5 @@
-import pandas_datareader.data as web
+# import pandas_datareader.data as web
+import numpy as np
 import datetime
 import dash
 import dash_core_components as dcc
@@ -8,33 +9,52 @@ from dash.dependencies import Input, Output
 app = dash.Dash()
 
 app.layout = html.Div(children=[
-    html.Div(children='''
-        Symbol to graph:
-    '''),
-    dcc.Input(id='input', value='', type='text'),
-    html.Div(id='output-graph'),
+    html.Div(children='''wasauchimmer:'''),
+        # dcc.Interval(
+        #     id='interval_component',
+        #     interval=50000,
+        #     n_intervals=0
+        #     )
+        html.Div(id='output-graph'),
+
+    # html.Div(children='''Symbol to graph:'''),
+    #     # dcc.Interval(
+    #     #     id='interval_component',
+    #     #     interval=50000,
+    #     #     n_intervals=0
+    #     #     )
+    #     html.Button('Click Me', id='my-button'),
+
 ])
+
+# Funktio zum auslesen des Datenfiles
+def read_from_file():
+    data = np.loadtxt('/home/hjk/test.md')
+    xdata = data[:,0]
+    ydata = data[:,1]
+    return xdata, ydata
+
 
 @app.callback(
     Output(component_id='output-graph', component_property='children'),
-    [Input(component_id='input', component_property='value')]
+    # [Input('interval_component', 'n_intervals')]
+    [Input('my-button', 'n_clicks')]
 )
 def update_value(input_data):
-    start = datetime.datetime(2015, 1, 1)
-    end = datetime.datetime.now()
-    df = web.DataReader(input_data, 'google', start, end)
-
+    xdata, ydata = read_from_file()
+    print(xdata)
     return dcc.Graph(
         id='example-graph',
         figure={
             'data': [
-                {'x': df.index, 'y': df.Close, 'type': 'line', 'name': input_data},
+                {'x': xdata, 'y': ydata, 'type': 'bar', 'name': input_data},
             ],
-            'layout': {
-                'title': input_data
-            }
+            # 'layout': {
+            #     'title': input_data
+            # }
         }
     )
 
 if __name__ == '__main__':
     app.run_server(debug=True)
+
