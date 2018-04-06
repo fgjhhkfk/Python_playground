36db0177c802e3abf3aa7267644af1bb02fe87c1
fgjhhkfk <fgjhhkfk@googlemail.com>
2018-04-06 23:21:30 +0200

    alles wieder auf automatische aktualisierung zurueck, und mit einlesen eines Files mit Zeitstempel

--- test_dash.py
+++ test_dash.py
@@ -1,61 +1,43 @@
-# import pandas_datareader.data as web
 import numpy as np
-import datetime
+import datetime as dt
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
-from dash.dependencies import Input, Output, Event
+from dash.dependencies import Input, Output
 
 app = dash.Dash()
 
 app.layout = html.Div(children=[
-    # html.Div(children='''wasauchimmer:'''),
-    # dcc.Interval(
-    #         id='interval_component',
-    #         interval=10000,
-    #         n_intervals=0
-    #         ),
-    html.Div(id='output-graph'),
-    html.Button(id='submit', type='submit', children='ok'),
-
-    # html.Div(children='''Symbol to graph:'''),
-    #     # dcc.Interval(
-    #     #     id='interval_component',
-    #     #     interval=50000,
-    #     #     n_intervals=0
-    #     #     )
-    #     html.Button('Click Me', id='my-button'),
-
+    html.Div(children='''Hier ist die Ueberschrift!!!'''),
+    dcc.Interval(
+            id='interval_component',
+            interval=10000,
+            n_intervals=0
+            ),
+    html.Div(id='output-graph')
 ])
 
-# Funktio zum auslesen des Datenfiles
+
+# Funktion zum Auslesen des Datenfiles
 def read_from_file():
-    data = np.loadtxt('/home/hjk/test.md')
-    xdata = data[:,0]
-    ydata = data[:,1]
-    return xdata, ydata
+    datey = lambda x: dt.datetime.strptime(x,'%d.%m.%Y_%H:%M:%S')
+    D = np.loadtxt('/media/web/thermometer/temp.dat',delimiter=" ",usecols=[0],
+                dtype=dt.datetime,converters={0:datey})
+    Z = np.loadtxt('/media/web/thermometer/temp.dat',delimiter=" ",usecols=[1])
+    return D, Z
 
 
 @app.callback(
     Output(component_id='output-graph', component_property='children'),
-    # [Input('interval_component', 'n_intervals')],
-    [],
-    [],
-    [Event('submit', 'click')],
-    # [Input('my-button', 'n_clicks')]
-)
+    [Input('interval_component', 'n_intervals')])
 def update_value(input_data):
     xdata, ydata = read_from_file()
-    print(xdata)
     return dcc.Graph(
         id='example-graph',
         figure={
             'data': [
                 {'x': xdata, 'y': ydata, 'type': 'line', 'name': input_data},
-            ],
-            # 'layout': {
-            #     'title': input_data
-            # }
+            ]
         }
     )
 
