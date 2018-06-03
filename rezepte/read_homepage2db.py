b2bc90c6bcce8dcec8041e8acaa7222ef31e486e
fgjhhkfk <fgjhhkfk@googlemail.com>
2018-06-03 22:46:55 +0200

    Suche funktioniert; Eingabefeld auf der Startseite vorbereitet, funktioniert aber noch nicht.

--- rezepte/read_homepage2db.py
+++ rezepte/read_homepage2db.py
@@ -5,18 +5,17 @@ jeden link, jedes bild, jeden Titel in die DB schreiben
 jedem link folgen, und zutaten und zubereitung auch in die db schreiben'''
 
 
-from bs4 import BeautifulSoup, SoupStrainer
-import re
+from bs4 import BeautifulSoup
 
 import rezepte as rz
 
-rz.create_recipie_db('rezepte.sqlite')
+rz.create_recipie_db('foobar.sqlite')
 
 base_url = '/media/web/'
 with open(base_url + 'rezepte.html') as html_file:
     soup = BeautifulSoup(html_file, 'lxml')
     navigation = soup.find(class_='navigation')
-    navlinks=navigation.find_all('a')
+    navlinks = navigation.find_all('a')
 
 for navlink in navlinks:
     # print navlink.get('href')
@@ -29,34 +28,35 @@ for navlink in navlinks:
         rezepte = content.find_all('a')
         rezepte = rezepte[1::2]
         thumbnails = content.find_all('img')
+        kategorie = navlink.get('title')
+        tags = 'wasser'
         # for link in rezepte:
         for i in range(1, len(rezepte)):
-            # with open(base_url + link.get('href')) as html_file:
             with open(base_url + rezepte[i].get('href')) as html_file:
                 soup = BeautifulSoup(html_file, 'lxml')
                 zubereitung_raw = soup.find('zubereitung')
                 zubereitung = zubereitung_raw.find_all('p')[1:]
                 bild = zubereitung_raw.find('img')
                 zutaten = soup.find('zutaten')
-                print('\n')
-                # print link.get('href')
-                print(rezepte[i].get('href').split('/')[-1].split('.')[0])
-                print(rezepte[i].get('href'))
-                print(bild.get('src'))
-                print(thumbnails[i].get('src'))
-                print(zutaten)
-                print(zubereitung[1::])
+                # print('\n')
+                # print(rezepte[i].get('href').split('/')[-1].split('.')[0])
+                # print(rezepte[i].get('href'))
+                # print(bild.get('src'))
+                # print(thumbnails[i].get('src'))
+                # print(zutaten)
+                # print(zubereitung[1::])
+                # print(kategorie)
                 # raw_input("press somethin to continue ...")
-                # write2db(titel, link, bild, thumbnail, zutaten, zubereitung):
-                rz.write2db(rezepte[i].get('href').split('/')[-1].split('.')[0],\
-                    rezepte[i].get('href'),\
-                    bild.get('src'),\
-                    thumbnails[i].get('src'),\
-                    zutaten,\
-                    zubereitung[1::])
+                rz.write2db(rezepte[i].get('href').split('/')[-1].split('.')[0],
+                            rezepte[i].get('href'),
+                            bild.get('src'),
+                            thumbnails[i].get('src'),
+                            zutaten,
+                            zubereitung[1::],
+                            kategorie,
+                            tags)
 
 '''
-Bilder nicht als Link in die DB sondern als Bild
 Zubereitung und Zutaten muessen noch aufgehuebscht werden
 die Rezepte Kategorien zuordnen (main dolce ...), natuerlich in die DB
 Tags mit in die DB aufnehmen
--- rezepte/rezepte.py
+++ rezepte/rezepte.py
@@ -11,6 +11,8 @@ def create_recipie_db(filename):
     spalte5 = 'thumbnail'
     spalte6 = 'zutaten'
     spalte7 = 'zubereitung'
+    spalte8 = 'kategorie'
+    spalte9 = 'tags'
     spalte1_typ = 'INTEGER PRIMARY KEY'
     spalte2_typ = 'STRING'
     spalte3_typ = 'STRING'
@@ -18,44 +20,60 @@ def create_recipie_db(filename):
     spalte5_typ = 'STRING'
     spalte6_typ = 'STRING'
     spalte7_typ = 'STRING'
+    spalte8_typ = 'STRING'
+    spalte9_typ = 'STRING'
 
 
     # verbindung erstellen
     conn = sqlite3.connect(sqlite_file)
     c = conn.cursor()
 
-    c.execute('CREATE TABLE {tn}({nf1} {ft1}, {nf2} {ft2}, {nf3} {ft3}, {nf4} {ft4}, {nf5} {ft5}, {nf6} {ft6}, {nf7} {ft7})'\
+    c.execute('CREATE TABLE {tn}({nf1} {ft1},\
+                                 {nf2} {ft2},\
+                                 {nf3} {ft3},\
+                                 {nf4} {ft4},\
+                                 {nf5} {ft5},\
+                                 {nf6} {ft6},\
+                                 {nf7} {ft7},\
+                                 {nf8} {ft8},\
+                                 {nf9} {ft9})'\
             .format(tn=tabellenname, nf1=spalte1, ft1=spalte1_typ,\
                                     nf2=spalte2, ft2=spalte2_typ,\
                                     nf3=spalte3, ft3=spalte3_typ,\
                                     nf4=spalte4, ft4=spalte4_typ,\
                                     nf5=spalte5, ft5=spalte5_typ,\
                                     nf6=spalte6, ft6=spalte6_typ,\
-                                    nf7=spalte7, ft7=spalte7_typ))
+                                    nf7=spalte7, ft7=spalte7_typ,\
+                                    nf8=spalte8, ft8=spalte8_typ,\
+                                    nf9=spalte9, ft9=spalte9_typ))
 
     conn.commit()
     conn.close()
 
 
-def write2db(titel, link, bild, thumbnail, zutaten, zubereitung):
+def write2db(titel, link, bild, thumbnail, zutaten, zubereitung, kategorie, tags):
     spalte2 = 'titel'
     spalte3 = 'link'
     spalte4 = 'bild'
     spalte5 = 'thumbnail'
     spalte6 = 'zutaten'
     spalte7 = 'zubereitung'
+    spalte8 = 'kategorie'
+    spalte9 = 'tags'
 
-    db = sqlite3.connect('rezepte.sqlite')
+    db = sqlite3.connect('foobar.sqlite')
     c = db.cursor()
-    c.execute('''INSERT INTO Rezepte({nf1}, {nf2}, {nf3}, {nf4}, {nf5}, {nf6})\
-            VALUES("{v1}","{v2}","{v3}","{v4}","{v5}","{v6}")'''\
+    c.execute('''INSERT INTO Rezepte({nf1}, {nf2}, {nf3}, {nf4}, {nf5}, {nf6}, {nf7}, {nf8})\
+            VALUES("{v1}","{v2}","{v3}","{v4}","{v5}","{v6}","{v7}","{v8}")'''\
             .format(\
             nf1=spalte2, v1=titel,\
             nf2=spalte3, v2=link,\
             nf3=spalte4, v3=bild,\
             nf4=spalte5, v4=thumbnail,\
             nf5=spalte6, v5=zutaten,\
-            nf6=spalte7, v6=zubereitung))
+            nf6=spalte7, v6=zubereitung,\
+            nf7=spalte8, v7=kategorie,\
+            nf8=spalte9, v8=tags))
 
     db.commit()
 
